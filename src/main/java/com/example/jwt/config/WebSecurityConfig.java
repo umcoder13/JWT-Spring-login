fc109259d3a4a6229af03e2ce5067dd7faff0167
umcoder13 <junehoum@gmail.com>
2022-09-30 19:12:58 +0900

    config 재 수정

--- src/main/java/com/example/jwt/config/WebSecurityConfig.java
+++ src/main/java/com/example/jwt/config/WebSecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.CorsUtils;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @RequiredArgsConstructor
@@ -50,6 +51,7 @@ public class WebSecurityConfig {
                 .and()
                 .authorizeRequests()
                 .antMatchers( "/auth/**", "/article/**", "/recommend/**", "/comment/**").permitAll()
+                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
                 .anyRequest().authenticated()
 
                 .and()
